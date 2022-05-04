ee5ef69f5cc1a3818fea47ae5fa55f6439e43217
srbchhabra <47674454+srbchhabra@users.noreply.github.com>
2022-05-04 12:49:56 +0530

    done

---
+++ src/Components/Temp.js
@@ -0,0 +1,4 @@
+const array=[1,2,3,4,5]
+array.filter(e=>e===2)
+const filtered=array.filter(e=>e!==2)
+console.log(filtered)
--- src/Components/Video.jsx
+++ src/Components/Video.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {storage} from './firebase'
-import {ref, uploadBytesResumable} from "firebase/storage";
+import {getDownloadURL, ref, uploadBytesResumable} from "firebase/storage";
 
 function Video() {
 
@@ -12,8 +12,15 @@ uploadTask.on('state_changed',(snapshot)=>{
     const progress=(snapshot.bytesTransferred / snapshot.totalBytes)*100;
 console.log("Upload is" + progress + "%done" );
 
-})
+}).then(snapshot=>{getDownloadURL(snapshot.ref).then(url=>{console.log(url)})})
     }
+    const keyGenerator = () => {
+      return (
+        Math.random().toString(36).substring(2, 15) +
+        Math.random().toString(36).substring(2, 15)
+      );
+    };
+    console.log(keyGenerator())
   return (
     <div>video
         <input type="file" onChange={uploadFile} />
